7d946138c1eada41f9321f176428197fa463b3a9
the 14th web <dennis.itua1@gmail.com>
2019-09-30 00:01:39 -0400

    app.js updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,96 @@
-$(document).ready(function(){
-    var animals = ["dog", "cat", "bird", "frog", "goldfish", "badger", "bear", "sloth", "bat", "camel", "bee", "crocodiles", "gorillas", "lions", "tigers", "monkey", "kangaroo", "geese", "oxen", "zebra", "whales", "weasels", "swans", "squirrels"];
+//Initial array of movies	
+$(document).ready(function() {
 
-    // Add buttons for animal array
-    function renderButtons() {
-        $("#animal-buttons").empty();
-        for(var i = 0; i < animals.length; i++) {
-             $("#animal-buttons").append("<button class='btn btn-primary' data-animal='" + animals[i] + "'>" + animals[i] + "</button>");
-            // var a = $("<button>");
-            //  a.addclass("animal");
-            //  a.attr("data-name", animals[i]);
-            //  a.text(animals[i]);
-            //  $("#animal-buttons").append(a);
-        }
-    }
-    renderButtons();
-
-    $(".add-animal").on("click", function(){
-        event.preventDefault();
-        var animal = $("#animal-input").val().trim();
-        animals.push(animal);
-        renderButtons();
-        return;
-    });
-
-    $("button").on("click", function () {
-		var animal = $(this).attr("data-animal");
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            animal + "&api_key=12QiWZ7afZ51SrEggGq3YFKQZELe8Qbd&limit=10";
-            
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
-            
-            .then(function (response) {
-                var results = response.data;
-                $("#animals").empty();
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-
-                   var animalDiv = $("<div>");
-                   var rating = results[i].rating;
-                   var p = $("<p>").text("Rating: " + rating);
-                   var animalImg = $("<img>");
-
-                   animalImg.attr("src", results[i].images.fixed_height_still.url);
-                   animalImg.attr("data-still", results[i].images.original_still.url);
-                   animalImg.attr("data-animate", results[i].images.original.url);
-                   animalImg.attr("data-state", "still");
-                   animalImg.attr("class", "gif");
-                   animalDiv.append(p);
-                   animalDiv.append(animalImg);
-                   $("#animals").append(animalDiv);
-                }
-             }  
-        });
-    });
-        
-      
-        function changeState(){
-            var state = $(this).attr("data-state");
-            var animateImage = $(this).attr("data-animate");
-            var stillImage = $(this).attr("data-still");
-    
-            if (state === "still") {
-                $(this).attr("src", animateImage);
-                $(this).attr("data-state", "animate");
+    var animals = ["dog", "cat", "bird", "frog", "goldfish", "badger", "bear", "sloth", "bat", "camel", "bee", "crocodile", "gorilla", "lion", "tiger", "monkey", "kangaroo", "geese", "oxen", "zebra", "whale", "weasel", "swan", "squirrel"];	
+  
+    //  create topics array buttons
+    function renderButtons(){
+      $('#animal-buttons').empty();
+     
+  
+      for (var i = 0; i < animals.length; i++) {
+              //create all buttons
+              var a = $('<button class= "btn btn-primary btn m-1">');
+              a.addClass('animal');
+              a.attr('data-name', animals[i]);
+              a.text(animals[i]);
+              $('#animal-buttons').append(a);
             }
-    
-            else if (state === "animate") {
-                $(this).attr("src", stillImage);
-                $(this).attr("data-state", "still");
-            }
-        }
-    
-        $(document).on("click", ".gif", changeState);
-});
+          }    
+          renderButtons();
+  
+  //on button click
+  $(document).on('click', '.animal', function() {
+  
+      //new variable will log the text data from each button
+      var animal = $(this).html(); 
+      // console.log(animal);
+  
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=12QiWZ7afZ51SrEggGq3YFKQZELe8Qbd&limit=10";
+      // console.log(queryURL);
+  
+      // Creating an AJAX call for the specific animal button being clicked
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+  
+        var results = response.data;
+          //console.log(results);
+          //empties the div before adding more gifs
+         $('#animals').empty();
+          for ( var q = 0; q < results.length; q++) {
+                      var imageDiv = $('<div>');
+                      var imageView = results[q].images.fixed_height.url;
+                      var still = results[q].images.fixed_height_still.url;
+                          // console.log(imageView);  
+  
+          var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
+                      gifImage.attr('data-state', 'still');
+                      $('#animals').prepend(gifImage);
+                      gifImage.on('click', playGif);
+  
+          // Pulling ratings for each movie
+          var rating = results[q].rating;
+              // console.log(rating);
+          var displayRated= $('<p>').text("Rating: " + rating);
+          $('#animals').prepend(displayRated);
+    } 
+  
+  }); 
+  
+          //function to stop and animate gifs
+          function playGif() { 
+                      var state = $(this).attr('data-state');
+                      // console.log(state);
+                   if (state == 'still'){
+                       $(this).attr('src', $(this).data('animate'));
+                        $(this).attr('data-state', 'animate');
+                   } else{
+                       $(this).attr('src', $(this).data('still'));
+                       $(this).attr('data-state', 'still');
+                      }
+  
+                  }
+  
+        }); 
+  
+            //adding new button to array
+          $(document).on('click', '.add-animal', function(){
+              if ($('#animal-input').val().trim() == ''){
+                alert('Type in the name of an animal, and click on the SUBMIT button');
+             }
+             else {
+              var animalPlace = $('#animal-input').val().trim();
+              animals.push(animalPlace);
+              $('#animal-input').val("");
+              renderButtons();
+            
+              return false;
+             
+              }
+  
+          });
+                        
+  
+          });
